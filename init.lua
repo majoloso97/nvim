91714f8e71d68058258ae32961d85b94b23938e4
majoloso97 <majoloso97@gmail.com>
2024-07-04 23:39:43 +0200

    Test remove line

--- init.lua
+++ init.lua
@@ -883,7 +883,6 @@ require("lazy").setup({
 		dependencies = {
 			"nvim-lua/plenary.nvim", -- required
 			"sindrets/diffview.nvim", -- optional - Diff integration
-
 			-- Only one of these is needed, not both.
 			"nvim-telescope/telescope.nvim", -- optional
 		},
