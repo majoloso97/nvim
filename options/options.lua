4a82f99da093f653180d3d5241e010f7b5f1ea91
majoloso97 <majoloso97@gmail.com>
2024-07-04 19:35:09 +0200

    Add basic vim.opt options to separate file

---
+++ options/options.lua
@@ -0,0 +1,60 @@
+local opt = vim.opt
+
+-- Make line numbers default
+opt.number = true
+-- Set line numbers to relative
+opt.relativenumber = true
+
+-- Enable mouse mode
+opt.mouse = "a"
+
+-- Don't show the mode, since it's already in the status line
+opt.showmode = false
+
+-- Sync clipboard between OS and Neo
+opt.clipboard = "unnamedplus"
+
+-- Enable break indent
+opt.breakindent = true
+
+-- Save undo history
+opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+opt.ignorecase = true
+opt.smartcase = true
+
+-- Keep signcolumn on by default
+opt.signcolumn = "yes"
+
+-- Decrease update time
+opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+opt.timeoutlen = 300
+
+-- Configure how new splits should be opened
+opt.splitright = true
+opt.splitbelow = true
+
+-- Sets how neowill display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+opt.list = true
+opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+-- Preview substitutions live, as you type!
+opt.inccommand = "split"
+
+-- Show which line your cursor is on
+opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+opt.scrolloff = 10
+
+-- [[ Basic Keymaps ]]
+--  See `:help keymap.set()`
+
+-- Set highlight on search, but clear on pressing <Esc> in normal mode
+opt.hlsearch = true
