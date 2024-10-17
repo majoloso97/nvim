4f38d5396130b4654a28ed8686e7ac76386c7d4c
Marvin López Osorio <majoloso97@gmail.com>
2024-10-17 20:39:53 +0200

    feat: add bufdel plugin for deleting buffers without altering windows layouts

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,9 @@
 local Plugins = {
-  {'tpope/vim-sleuth'},
-  -- "gc" to comment visual regions/lines
-  { "numToStr/Comment.nvim", opts = {} },
+	{ "tpope/vim-sleuth" },
+	-- "gc" to comment visual regions/lines
+	{ "numToStr/Comment.nvim", opts = {} },
+	-- delete buffers with no change in layout
+	{ "ojroques/nvim-bufdel", opts = { next = "cycle" } },
 }
 
 return Plugins
