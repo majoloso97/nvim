de8bef93e2ed6bf63a2fd46f3e5e04496a781d87
Marvin López Osorio <majoloso97@gmail.com>
2024-10-27 23:54:03 +0100

    feat: add c lsp and enable it

--- lua/plugins/autoformat.lua
+++ lua/plugins/autoformat.lua
@@ -18,7 +18,7 @@ return {
 				-- Disable "format_on_save lsp_fallback" for languages that don't
 				-- have a well standardized coding style. You can add additional
 				-- languages here or re-enable it for the disabled ones.
-				local disable_filetypes = { c = true, cpp = true }
+				local disable_filetypes = { cpp = true }
 				return {
 					timeout_ms = 500,
 					lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
@@ -29,6 +29,7 @@ return {
 				-- Conform can also run multiple formatters sequentially
 				python = { "isort", "ruff_format" },
 				go = { "gopls" },
+				c = { "clang" },
 				--
 				-- You can use a sub-list to tell conform to run *until* a formatter
 				-- is found.
