f2b360828ef0e9f778481b08021d242bfc7b0481
Marvin López Osorio <majoloso97@gmail.com>
2025-04-08 11:02:02 +0200

    feat: add prettier as formatter for js, ts

--- lua/plugins/autoformat.lua
+++ lua/plugins/autoformat.lua
@@ -34,7 +34,8 @@ return {
 				--
 				-- You can use a sub-list to tell conform to run *until* a formatter
 				-- is found.
-				-- javascript = { { "prettierd", "prettier" } },
+				javascript = { { "prettier" } },
+				tpyescript = { { "prettier" } },
 			},
 			formatters = {
 				sql_formatter = {
