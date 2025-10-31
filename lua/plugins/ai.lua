48ab1311c6e481984f5e033a881e8c81ddfd4f03
Marvin López Osorio <majoloso97@gmail.com>
2025-10-31 13:31:59 +0100

    feat: add opencode plugin base config

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -22,4 +22,30 @@ return {
 			})
 		end,
 	},
+	{
+		"sudo-tee/opencode.nvim",
+		config = function()
+			require("opencode").setup({})
+		end,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{
+				"MeanderingProgrammer/render-markdown.nvim",
+				opts = {
+					anti_conceal = { enabled = false },
+					file_types = { "markdown", "opencode_output" },
+				},
+				ft = { "markdown", "opencode_output" },
+			},
+			-- Optional, for file mentions and commands completion, pick only one
+			-- "saghen/blink.cmp",
+			"hrsh7th/nvim-cmp",
+
+			-- Optional, for file mentions picker, pick only one
+			-- "folke/snacks.nvim",
+			"nvim-telescope/telescope.nvim",
+			-- 'ibhagwan/fzf-lua',
+			-- 'nvim_mini/mini.nvim',
+		},
+	},
 }
